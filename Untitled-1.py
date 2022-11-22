63d3ed1dc5090fe70399164e20fdafd7e7ac0609
Tom Brooks <tbrooks2706@gmail.com>
2022-11-22 08:47:03 +0000

    Added greeting, narrowed import

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,10 @@
-from datetime import *
+from datetime import datetime
 def now():
     for iteration in range (0, 10):
         print(datetime.now())
+
+def greeting(name):
+    print("Hello, "+name+"!") 
+
+greeting("Tom")
 now()
