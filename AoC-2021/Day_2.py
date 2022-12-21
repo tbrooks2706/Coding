b7c9e7420e4a578a50709a6f85d63aad6ace4c03
Tom Brooks <tbrooks2706@gmail.com>
2022-12-21 09:04:28 +0000

    prep for several days

---
+++ AoC-2021/Day_2.py
@@ -0,0 +1,4 @@
+import general_functions
+
+init_list = general_functions.read_file(r"C:\Users\Tom\OneDrive\Documents\Tom's Stuff\Hobbies\Coding\AoC-2021\Day_2.txt")
+test_list = general_functions.read_file(r"C:\Users\Tom\OneDrive\Documents\Tom's Stuff\Hobbies\Coding\AoC-2021\Day_2_test.txt")
---
+++ AoC-2021/Day_3.py
@@ -0,0 +1,4 @@
+import general_functions
+
+init_list = general_functions.read_file(r"C:\Users\Tom\OneDrive\Documents\Tom's Stuff\Hobbies\Coding\AoC-2021\Day_3.txt")
+test_list = general_functions.read_file(r"C:\Users\Tom\OneDrive\Documents\Tom's Stuff\Hobbies\Coding\AoC-2021\Day_3_test.txt")
