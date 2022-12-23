049d82a19c41b15aee6e9b419799fec2291a877c
Tom Brooks <tbrooks2706@gmail.com>
2022-12-23 20:12:40 +0000

    day 4 - mystery

--- AoC-2021/Day_4.py
+++ AoC-2021/Day_4.py
@@ -18,6 +18,9 @@ class Board:
     def __init__(self, row_list) -> None:
         self.row_list = row_list
         self.column_list = self.create_columns()
+        self.sum_total = self.sum_values()[0]
+        self.sum_unmarked = self.sum_values()[1]
+        self.sum_marked = self.sum_values()[2]
     
     def create_columns(self):
         column_list = []
@@ -54,16 +57,42 @@ class Board:
             row_marked.append(count)
         return [row_marked, column_marked]
 
+    def sum_values(self):
+        sum = 0
+        unmarked = 0
+        marked = 0
+        print(self.row_list)
+        for row in self.row_list:
+            for value in row:
+                #print(value)
+                if "#" not in value:
+                    unmarked += int(value)
+                    sum += int(value)
+                else:
+                    marked += int(value[1:])
+                    sum += int(value)
+        return [sum, unmarked, marked]
+
 
 example_board = Board([['22', '13', '17', '11', '0'], ['8', '2', '23', '4', '24'], ['21', '9', '14', '16', '7'], ['6', '10', '3', '18', '5'], ['1', '12', '20', '15', '19']])
 example_board.mark_board("13")
-#print(example_board.column_list)
-print(example_board.count_marked())
+print(example_board.row_list)
+#print(example_board.count_marked())
+print("unmarked",example_board.sum_unmarked)
+print("marked",example_board.sum_marked)
+print("total",example_board.sum_total)
 
+#PROBLEM: the sum_values function is calling a version of row_list which isn't showing the markup
+#even though when you call row_list separately, it shows as marked
+#no idea why - need to figure this out
 
 #not sure if we need this class
 class Number_Draw:
     def __init__(self, number, seq) -> None:
         self.number = number
         self.seq = seq
+
+#for each number draw
+    #go through entire list, and mark each board
+    #check number marked, if any 5, break
 
