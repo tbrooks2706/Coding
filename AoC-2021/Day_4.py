4d71f920e116389ce666dfd7a84b3875ae279c70
Tom Brooks <tbrooks2706@gmail.com>
2022-12-25 22:31:20 +0000

    part 1 done

--- AoC-2021/Day_4.py
+++ AoC-2021/Day_4.py
@@ -4,7 +4,7 @@ import copy
 init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\Coding\AoC-2021\Day_4.txt")
 test_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\Coding\AoC-2021\Day_4_test.txt")
 #print(test_list)
-draw_order = init_list[0].split(",")
+draw_order = test_list[0].split(",")
 #print(general_functions.check_for_duplicates(draw_order))
 boards = [item.split() for item in test_list[2:]]
 #print(draw_order)
@@ -45,6 +45,7 @@ class Board:
     def count_marked(self):
         column_marked = []
         row_marked = []
+        board_win = False
         for column in self.column_list:
             count = 0
             for value in column:
@@ -57,7 +58,9 @@ class Board:
                 if "#" in value:
                     count += 1
             row_marked.append(count)
-        return [row_marked, column_marked]
+        if (5 in row_marked) or (5 in column_marked):
+            board_win = True
+        return [row_marked, column_marked, board_win]
 
     def sum_values(self):
         sum = 0
@@ -75,23 +78,49 @@ class Board:
                     sum += int(value[1:])
         return [sum, unmarked, marked]
 
-example_board = Board([['22', '13', '17', '11', '0'], ['8', '2', '23', '4', '24'], ['21', '9', '14', '16', '7'], ['6', '10', '3', '18', '5'], ['1', '12', '20', '15', '19']])
-for num in ["1", "21", "17", "2"]:
-    example_board.mark_board(num)
+#example_board = Board([['22', '13', '17', '11', '0'], ['8', '2', '23', '4', '24'], ['21', '9', '14', '16', '7'], ['6', '10', '3', '18', '5'], ['1', '12', '20', '15', '19']])
+#example_board = Board(boards[0:5])
+#for num in ["21", "8", "22", "1", "5"]:
+#    example_board.mark_board(num)
 #print(example_board.row_list)
-print(example_board.count_marked())
-print("unmarked",example_board.sum_values()[1])
-print("marked",example_board.sum_values()[2])
-print("total",example_board.sum_values()[0])
-print("seq",example_board.seq)
-print("number called",example_board.number_called)
-print("score",example_board.number_called * example_board.sum_values()[1])
+#print(example_board.count_marked())
+#print("unmarked",example_board.sum_values()[1])
+#print("marked",example_board.sum_values()[2])
+#print("total",example_board.sum_values()[0])
+#print("seq",example_board.seq)
+#print("number called",example_board.number_called)
+#print("score",example_board.number_called * example_board.sum_values()[1])
 
-#not sure if we need this class
 class Number_Draw:
-    def __init__(self, number, seq) -> None:
+    def __init__(self, number) -> None:
         self.number = number
-        self.seq = seq
+
+    def mark_all_numbers(self, input_boards):
+        #go through each line, mark the number if it's there, then overwrite that line the original list (boards) with the new line
+        for line in input_boards:
+            for index in range(len(line)):
+                if line[index] == self.number:
+                    line[index] = "#"+line[index]
+
+#example_number_draw = Number_Draw("10")
+#example_number_draw.mark_all_numbers(boards)
+#print(boards)
+
+        
+class Boards_List:
+    def __init__(self, input_list) -> None:
+        self.boards_list = input_list
+        self.board_starts = self.determine_board_starts()
+
+    def determine_board_starts(self):
+        starts_list = []
+        for x in range(len(self.boards_list)):
+            if x % 6 == 0:
+                starts_list.append(x)
+        return starts_list
+
+example_boards_list = Boards_List(boards)
+#print(example_boards_list.board_starts)
 
 #for each number draw
     #go through entire list, and mark each board
@@ -102,4 +131,26 @@ class Number_Draw:
     #go through number list, mark board while no row or column = 5
     #when loop finished, do next board
 #then find board which has the lowest seq value, and output score for it
+
+def get_winning_board(draw_list, board_list):
+    start_points = Boards_List(board_list).board_starts
+    end_now = False
+    winning_board = 0
+    for draw in draw_list:
+        this_draw = Number_Draw(draw)
+        this_draw.mark_all_numbers(board_list)
+        for num in range(len(board_list)):
+            if num in start_points:
+                this_board = Board(board_list[num:num+5])
+                if this_board.count_marked()[2] == True:
+                    end_now = True
+                    winning_board = num
+                    winning_score = this_board.sum_values()[1] * int(draw)
+        if end_now == True:
+            break
+    return {"board start": winning_board, "score": winning_score}
+
+winning_board = get_winning_board(draw_order, boards)
+#answer to part 1
+print(winning_board)
 
