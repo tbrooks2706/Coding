71da03459c08354aa11632b07e6e4006cc43bf74
Tom Brooks <tbrooks2706@gmail.com>
2023-01-13 23:22:47 +0000

    day 7 complete

--- AoC-2021/Day_7.py
+++ AoC-2021/Day_7.py
@@ -8,11 +8,10 @@ def format_list(input_list):
     return [int(x.strip()) for x in input_list[0].split(",")]
 
 working_list = format_list(test_list)
-#print(working_list)
 
-#numbers are horizontal position of each crab
+#numbers in input are horizontal position of each crab
 #they can only move horizontally
-#you have to align them all on one number
+#you have to align them all on one number (get them all to eg. 3 from wherever they start)
 #each movement by one crab by one position, costs one fuel (for a crab to move from 16 to 3 costs 13)
 #how much fuel will they spend, aligning to the position that costs the least fuel possible?
 
@@ -20,7 +19,7 @@ working_list = format_list(test_list)
 #check every position number in that range
 #for each position:
     #run through all crabs, and keep a running total of how much fuel it would cost them all to get there
-    #add range number:total fuel to a dictionary
+    #add position:total fuel to a dictionary
 
 class Position:
     def __init__(self, number, input_list) -> None:
@@ -60,24 +59,21 @@ def find_all_fuels(input_list, method):
             return ValueError
     return fuel_dict
 
-#print(full_dictionary_2)
-
 def find_lowest_fuel(input_dict):
     lowest_fuel = min(input_dict.values())
     for key, value in input_dict.items():
         if value == lowest_fuel:
             return [key, value]
 
-print(datetime.datetime.now())
+#part 1
 full_dictionary_1 = find_all_fuels(working_list, "standard")
 lowest = find_lowest_fuel(full_dictionary_1)
 print(lowest)
-print(datetime.datetime.now())
-#full_dictionary_2 = find_all_fuels(working_list, "increasing")
-#lowest2 = find_lowest_fuel(full_dictionary_2)
-#print(lowest2)
-print(datetime.datetime.now())
 
+#part 2
+full_dictionary_2 = find_all_fuels(working_list, "increasing")
+lowest2 = find_lowest_fuel(full_dictionary_2)
+print(lowest2)
 
 
 
