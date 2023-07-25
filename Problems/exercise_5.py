bada36f08c796a3e406a501aa4e4cbd3059a4984
Tom Brooks <tbrooks2706@gmail.com>
2023-07-25 22:09:12 +0100

    first 5 exercises

---
+++ Problems/exercise_5.py
@@ -0,0 +1,5 @@
+#https://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(set([x for x in a if x in b]))
